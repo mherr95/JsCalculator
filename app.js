0e5de48cbe7f41f3b1cbbebbc13b8c60efcca53c
Michael Herr <michaelherr@Michaels-MacBook-Pro-2.local>
2022-05-18 21:53:40 -0400

    working on css

---
+++ app.js
@@ -0,0 +1,10 @@
+"usestrict";
+
+const input = document.querySelector('.entry');
+const result = document.querySelector('.answer')
+const onBtn = document.querySelector('.on')
+
+
+const on= () => {
+    on.forEach()
+}
